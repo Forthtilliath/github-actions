5976b388871b5658108fd345286a947914cce5a9
Forthtilliath <forth.developer@gmail.com>
2023-06-01 23:28:41 +0200

    undo success test

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// export function sum(a, b) {
-//   return a + b;
-// }
-
-export function sum(...arr) {
-  return arr.reduce((sum, curr) => sum + curr, 0);
+export function sum(a, b) {
+  return a + b;
 }
+
+// export function sum(...arr) {
+//   return arr.reduce((sum, curr) => sum + curr, 0);
+// }
 
 sum(1, 2);
 sum(1, 2, 3);
